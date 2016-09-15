39cdee8b25be24a4a2996ffe5565af2bf34c1fef
Kyle Sarre <ksarre2@lsu.edu>
2016-09-15 16:38:34 -0500

    Pushing Prototype
    Testing to see if unnecessary auto generated Unity files are not pushed
    to repo

---
+++ chess prototype/Assets/Scripts/Cell.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cell : MonoBehaviour 
+{
+	public GameObject myPiece;
+	Vector3 cellCenter;
+	public bool isActive;
+
+	// Use this for initialization
+	void Start () 
+	{
+		cellCenter = CellCenter;
+		isActive = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+	public void toggleActive()
+	{
+		isActive = (isActive == true) ? isActive = false : isActive = true;
+	}
+
+	public GameObject MyPiece
+	{
+		get{ return myPiece; }
+		set{ myPiece = value; }
+	}
+	public Vector3 CellCenter
+	{
+		get{ return cellCenter;}
+		private set
+		{
+			Vector3 origin = transform.position;
+			Vector3 midPoint = new Vector3(origin.x + Grid.distanceBetweenTiles, origin.y + Grid.distanceBetweenTiles, origin.z)*(1/2);
+			cellCenter = midPoint;
+		}
+	}
+}
---
+++ chess prototype/Assets/Scripts/Interfaces.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IPiece
+{
+	int Position { get; set;}
+	void move ();
+}
