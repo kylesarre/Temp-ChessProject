92d0d4370f987359cd9a7636d37e26b5cc30d03d
Kyle Sarre <ksarre2@lsu.edu>
2016-10-06 18:11:05 -0500

    Implemented Piece Selection with highlighting
    Pieces should now be selectable by hovering the mouse cursor and left
    clicking the piece. Pieces can be deselected by right clicking while
    having a piece selected. Highlighting doesn't seem to work for all of
    the pieces, not sure why.

--- chess prototype/Assets/Scripts/GameController.cs
+++ chess prototype/Assets/Scripts/GameController.cs
@@ -4,13 +4,15 @@ using System.Collections;
 public class GameController : MonoBehaviour 
 {
 	private static GameController instance;
-
 	public GameObject mainCamera;
 	public GameObject selectedPiece;
 	public GameObject grid;
 	private PlayerController player;
 	public GameStates curGameState;
 	public TurnStates curTurnState;
+	private int selectionX;
+	private int selectionY;
+
 
 	public enum GameStates
 	{
@@ -26,10 +28,14 @@ public class GameController : MonoBehaviour
 
 	void awake()
 	{
-		if (instance == null)
+		if (instance == null) 
+		{
 			instance = this;
+			DontDestroyOnLoad (transform.gameObject);
+		}
 		else
 			DestroyImmediate (this.gameObject);
+		
 	}
 
 	// Use this for initialization
@@ -37,27 +43,113 @@ public class GameController : MonoBehaviour
 	{
 		mainCamera = (GameObject)Instantiate (Resources.Load ("main_Camera"), new Vector3 (0, 0, 0), Quaternion.identity);		
 		curGameState = GameStates.GAME_START;
+		curTurnState = TurnStates.DEFAULT;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if (curGameState == GameStates.GAME_START) {
+		switch (curGameState) 
+		{
+			case GameStates.MAIN_MENU:
+			break;
+			case GameStates.GAME_START:
 			grid = (GameObject)Instantiate (Resources.Load ("obj_grid"), new Vector3 (0, 0, 0), Quaternion.identity);
 			// instantiate our two players
 			// set random player to white, other to black
 			curGameState = GameStates.IN_GAME;
-		} 
-		else if (curGameState == GameStates.IN_GAME) 
-		{
+			break;
+			case GameStates.IN_GAME:
 			CenterCameraToBoard();
-			startTurn ();
+			switch (curTurnState)
+			{
+				case TurnStates.TURN_START:
+				curTurnState = TurnStates.CAN_SELECT;
+				break;
+				case TurnStates.CAN_SELECT:
+				UpdateCursorPos ();
+				markCell ();
+				selectPiece ();
+				break;
+				case TurnStates.CAN_MOVE:
+				UpdateCursorPos ();
+				deselectPiece ();
+				break;
+				case TurnStates.DEFAULT:
+				curTurnState = TurnStates.TURN_START;
+				break;
+			}
+			break;
+			case GameStates.PAUSE:
+			break;
+			case GameStates.GAME_END:
+			break;
+
 		}
 	}
 
-	private void startTurn()
+	private void UpdateCursorPos()
 	{
-		curTurnState = TurnStates.TURN_START;
+		if (!mainCamera) 
+		{
+			Debug.Log ("No main camera, exiting.");
+			return;
+		}
+		RaycastHit2D hit;
+		hit = Physics2D.Raycast (Camera.main.ScreenToWorldPoint (Input.mousePosition), Vector2.zero, LayerMask.GetMask ("bottom"));
+		if (hit) {
+			Debug.Log (hit.point);
+			selectionX = Mathf.FloorToInt (hit.point.x);
+			selectionY = Mathf.FloorToInt (hit.point.y);
+		} 
+		else 
+		{
+			selectionX = -1;
+			selectionY = -1;
+		}
+	}
+	public void makeSelection()
+	{
+		
+	}
+	public void selectPiece()
+	{
+		float gridOriginX = grid.GetComponent<Transform> ().position.x;
+		float gridOriginY = grid.GetComponent<Transform> ().position.y;
+		Grid scr_Grid = grid.GetComponent<Grid> ();
+		bool mouseClicked = Input.GetMouseButtonDown (0);
+		if (mouseClicked)
+			Debug.Log ("Left mouse button pressed");
+		if (selectionX >= gridOriginX &&
+		    selectionX < gridOriginX + scr_Grid.NumOfColumns &&
+		    selectionY >= gridOriginY &&
+		    selectionY < gridOriginY + scr_Grid.NumOfRows &&
+			mouseClicked) 
+		{
+			Cell scr_Cell = scr_Grid.grid [selectionY, selectionX].GetComponent<Cell>();
+			selectedPiece = scr_Cell.MyPiece;
+			if (selectedPiece != null) 
+			{
+				curTurnState = TurnStates.CAN_MOVE;
+			}
+
+		}
+	}
+	public void deselectPiece()
+	{
+		bool mouseClicked = Input.GetMouseButtonDown (1);
+		if (mouseClicked) 
+		{
+			Debug.Log ("Right mouse button pressed");	
+			selectedPiece = null;
+			curTurnState = TurnStates.CAN_SELECT;
+		}
+	}
+	public void markCell()
+	{
+		Debug.DrawLine (Vector3.up*selectionY + Vector3.right*selectionX,
+			Vector3.up*(selectionY+1f) + Vector3.right*(selectionX+1f),
+			Color.red);
 	}
 
 	private void CenterCameraToBoard()
---
+++ chess prototype/Assets/Scripts/HightlightObject.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HightlightObject : MonoBehaviour 
+{
+	private Color startColor;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseEnter()
+	{
+		startColor = GetComponent<SpriteRenderer> ().material.color;
+		GetComponent<SpriteRenderer> ().material.color = Color.yellow;
+	}
+	void OnMouseExit()
+	{
+		GetComponent<SpriteRenderer> ().material.color = startColor;
+	}
+}
+	
