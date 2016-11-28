8ba8d2e0597b8ff6bc3e0012c0553e5ffeba68db
HannaCunningham <hcunni6@lsu.edu>
2016-11-28 15:59:01 -0600

    fixed menu colors

---
+++ chess_prototype/Assets/Menus/JulianMenuStuff/Scripts/PowerUpMenuController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpMenuController : MonoBehaviour
+{
+    public GameObject settingsMenu;
+    public GameObject ForfeitMenu;
+
+    public void Settings()
+    {
+        settingsMenu.SetActive(true);
+    }
+
+    public void Forfeit()
+    {
+        ForfeitMenu.SetActive(true);
+    }
+
+
+
+
+
+}
---
+++ chess_prototype/Assets/Menus/Spring Prefabs and Option Script/QuitOnClick.cs
@@ -0,0 +1,29 @@
+﻿/****************************************************************************************/
+/*
+/* FILE NAME: QuitOnClick.cs
+/*
+/* DESCRIPTION: Allows the application to be exited from a button click.
+/*
+/* REFERENCE:
+/*
+/*   DATE          BY         CHANGE REF  DESCRIPTION
+/* ======== ================ =========== ================================================
+/* 11/26/16 Hanna Cunningham             created script and connected it to the main menu
+/* 
+/*
+/*
+/*
+/****************************************************************************************/
+
+using UnityEngine;
+
+public class QuitOnClick : MonoBehaviour
+{
+    /**
+     * exits the game and closes the executable
+     */    
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+}
